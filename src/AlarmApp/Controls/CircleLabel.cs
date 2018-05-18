0b056c470ccb791ff724affda2ab5086c835f2c4
Neil <neil.malcolm@enkelt.co.uk>
2018-05-18 22:08:51 +0100

    Added CircleLabel and renderer

---
+++ src/AlarmApp/Controls/CircleLabel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AlarmApp.Controls
+{
+	/// <summary>
+	/// CircleLabel to display text within a circle
+	/// </summary>
+	public class CircleLabel : Label
+	{
+		
+	}
+}
---
+++ src/Droid/Renderers/AndroidCircleLabelRenderer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+using AlarmApp.Controls;
+using AlarmApp.Droid.Renderers;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics;
+using System.ComponentModel;
+using Android.Support.V4.Content.Res;
+using Android.Content;
+using XFColor = Xamarin.Forms.Color;
+
+[assembly: ExportRenderer(typeof(CircleLabel), typeof(AndroidCircleLabelRenderer))]
+namespace AlarmApp.Droid.Renderers
+{
+	public class AndroidCircleLabelRenderer : LabelRenderer
+	{
+		public AndroidCircleLabelRenderer(Context context) : base(context)
+		{
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				SetCircleBackground();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if(e.PropertyName == CircleLabel.BackgroundColorProperty.PropertyName)
+			{
+				if(Element.BackgroundColor != XFColor.Transparent)
+				{
+					SetCircleBackground();
+				}
+			}
+		}
+
+
+		void SetCircleBackground()
+		{
+			//Set our control background to round drawable and overlay our background color as long as our background color isn't transparent
+			if (Element.BackgroundColor != default(Xamarin.Forms.Color) && Element.BackgroundColor != Xamarin.Forms.Color.Transparent)
+			{
+				Control.Background = ResourcesCompat.GetDrawable(Resources, Resource.Drawable.circle_background, null);
+				Control.Background.SetColorFilter(Element.BackgroundColor.ToAndroid(), PorterDuff.Mode.Src);
+				Element.BackgroundColor = XFColor.Transparent;
+			}
+		}
+
+	}
+}
