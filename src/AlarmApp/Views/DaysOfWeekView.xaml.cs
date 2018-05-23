38c4ca54a7e1e5b210291c996b011c23f5b821ae
Neil <neil.malcolm@enkelt.co.uk>
2018-05-23 15:27:27 +0100

    Days of week display no longer uses CircleLabel control
    Displays days in "Mon, Tue, Wed" format instead

--- src/AlarmApp/Views/DaysOfWeekView.xaml.cs
+++ src/AlarmApp/Views/DaysOfWeekView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using AlarmApp.Models;
 using Xamarin.Forms;
 
@@ -7,6 +8,12 @@ namespace AlarmApp.Views
 {
 	public partial class DaysOfWeekView : ContentView
 	{
+		StringBuilder _sb;
+
+		string[] days = {
+			"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+		};
+
 		public DaysOfWeekView()
 		{
 			InitializeComponent();
@@ -21,14 +28,29 @@ namespace AlarmApp.Views
 			var alarm = ((Alarm)BindingContext);
 
 			var daysOfWeek = alarm.Days;
+			_sb = new StringBuilder();
 
-			MondayLabel.IsVisible = daysOfWeek.Monday;
-			TuesdayLabel.IsVisible = daysOfWeek.Tuesday;
-			WednesdayLabel.IsVisible = daysOfWeek.Wednesday;
-			ThursdayLabel.IsVisible = daysOfWeek.Thursday;
-			FridayLabel.IsVisible = daysOfWeek.Friday;
-			SaturdayLabel.IsVisible = daysOfWeek.Saturday;
-			SundayLabel.IsVisible = daysOfWeek.Sunday;
+			for (int i = 0; i < daysOfWeek.AllDays.Length; i++)
+			{
+				var hasDay = daysOfWeek.AllDays[i];
+				if(hasDay)
+				{
+					if (i > 0)
+					{
+						_sb.Append(", ");
+					}
+					_sb.Append(days[i]);
+				}
+			}
+
+			DaysLabel.Text = _sb.ToString();
+			//MondayLabel.IsVisible = daysOfWeek.Monday;
+			//TuesdayLabel.IsVisible = daysOfWeek.Tuesday;
+			//WednesdayLabel.IsVisible = daysOfWeek.Wednesday;
+			//ThursdayLabel.IsVisible = daysOfWeek.Thursday;
+			//FridayLabel.IsVisible = daysOfWeek.Friday;
+			//SaturdayLabel.IsVisible = daysOfWeek.Saturday;
+			//SundayLabel.IsVisible = daysOfWeek.Sunday;
 		}
 	}
 }
