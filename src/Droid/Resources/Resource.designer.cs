5da03bc1e05298b533c8c7eb4fcfd195557c814e
NeilMalcolm <neilmalcolmij@gmail.com>
2018-06-20 16:25:24 +0100

    Added settings page icons, and added drawable-mdpi folder
    relocated /drawable .png files to /drawable-mdpi

--- src/Droid/Resources/Resource.designer.cs
+++ src/Droid/Resources/Resource.designer.cs
@@ -2210,26 +2210,26 @@ namespace AlarmApp.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02011a
-			public const int avd_hide_password_1 = 2130837786;
+			// aapt resource value: 0x7f02011e
+			public const int avd_hide_password_1 = 2130837790;
 			
-			// aapt resource value: 0x7f02011b
-			public const int avd_hide_password_2 = 2130837787;
+			// aapt resource value: 0x7f02011f
+			public const int avd_hide_password_2 = 2130837791;
 			
-			// aapt resource value: 0x7f02011c
-			public const int avd_hide_password_3 = 2130837788;
+			// aapt resource value: 0x7f020120
+			public const int avd_hide_password_3 = 2130837792;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02011d
-			public const int avd_show_password_1 = 2130837789;
+			// aapt resource value: 0x7f020121
+			public const int avd_show_password_1 = 2130837793;
 			
-			// aapt resource value: 0x7f02011e
-			public const int avd_show_password_2 = 2130837790;
+			// aapt resource value: 0x7f020122
+			public const int avd_show_password_2 = 2130837794;
 			
-			// aapt resource value: 0x7f02011f
-			public const int avd_show_password_3 = 2130837791;
+			// aapt resource value: 0x7f020123
+			public const int avd_show_password_3 = 2130837795;
 			
 			// aapt resource value: 0x7f020056
 			public const int circle_background = 2130837590;
@@ -2798,11 +2798,11 @@ namespace AlarmApp.Droid
 			// aapt resource value: 0x7f020112
 			public const int notification_icon_background = 2130837778;
 			
-			// aapt resource value: 0x7f020118
-			public const int notification_template_icon_bg = 2130837784;
+			// aapt resource value: 0x7f02011c
+			public const int notification_template_icon_bg = 2130837788;
 			
-			// aapt resource value: 0x7f020119
-			public const int notification_template_icon_low_bg = 2130837785;
+			// aapt resource value: 0x7f02011d
+			public const int notification_template_icon_low_bg = 2130837789;
 			
 			// aapt resource value: 0x7f020113
 			public const int notification_tile_bg = 2130837779;
@@ -2817,7 +2817,19 @@ namespace AlarmApp.Droid
 			public const int save = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int splashimage = 2130837783;
+			public const int settings_alarm_tone = 2130837783;
+			
+			// aapt resource value: 0x7f020118
+			public const int settings_clock_format = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int settings_delete_alarms = 2130837785;
+			
+			// aapt resource value: 0x7f02011a
+			public const int settings_vibrate = 2130837786;
+			
+			// aapt resource value: 0x7f02011b
+			public const int splashimage = 2130837787;
 			
 			static Drawable()
 			{
