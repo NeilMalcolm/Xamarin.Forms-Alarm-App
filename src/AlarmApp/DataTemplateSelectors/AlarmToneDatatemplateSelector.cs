9c9d2983c0f037c10744b7ebd678b69cec490e54
NeilMalcolm <neilmalcolmij@gmail.com>
2018-07-01 12:53:32 +0100

    Default and custom tones can now be differentiated by DataTemplateSeelctor
    Improved the implementation of ContextActions by removing BindingContextChanged and using DataTemplateSelector

---
+++ src/AlarmApp/DataTemplateSelectors/AlarmToneDatatemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AlarmApp.Models;
+using Xamarin.Forms;
+
+namespace AlarmApp.DataTemplateSelectors
+{
+	public class AlarmToneDatatemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate DefaultTemplate { get; set; }
+		public DataTemplate CustomTemplate { get; set; }
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			var alarmTone = (AlarmTone)item;
+			return alarmTone.IsCustomTone ? CustomTemplate : DefaultTemplate; 
+		}
+	}
+}
