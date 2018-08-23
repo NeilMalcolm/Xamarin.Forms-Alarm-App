e85a047db91fe31324ded3704a09afcdac14ac88
Neil <neilmalcolmij@gmail.com>
2018-08-23 15:26:24 +0100

    Added MusicSelection control and renderer files

---
+++ src/AlarmApp/Controls/MusicSelectionButton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+namespace AlarmApp.Controls
+{ 
+	public class MusicSelectionButton : Button
+	{
+		public MusicSelectionButton()
+		{
+		}
+	}
+}
---
+++ src/Droid/Renderers/MusicSelectionRenderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AlarmApp.Droid.Renderers
+{
+	public class MusicSelectionRenderer
+	{
+		public MusicSelectionRenderer()
+		{
+		}
+	}
+}
